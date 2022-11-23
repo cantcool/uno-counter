b94bf79556e691d6c46148676a5c3a6d51ef8b21
cantcool <vyachaslau@gmail.com>
2022-11-23 23:50:41 +0100

    Fixes

--- src/App.js
+++ src/App.js
@@ -11,24 +11,24 @@ function App() {
     <div className="App">
       <div className="columns">
         <Paper elevation="1">
-          <TextField label="Player 1" color="primary" value={player1} type="number" onChange={e => setPlayer1(+e.target.value)} />
-          <Button variant="contained" color="success" onClick={() => setPlayer1(current => current + 1)}>+1</Button>
-          <Button variant="contained" color="success" onClick={() => setPlayer1(current => current + 5)}>+5</Button>
-          <Button variant="contained" color="success" onClick={() => setPlayer1(current => current + 10)}>+10</Button>
-          <Button variant="contained" color="success" onClick={() => setPlayer1(current => current + 20)}>+20</Button>
-          <Button variant="contained" color="success" onClick={() => setPlayer1(current => current + 50)}>+50</Button>
+          <TextField label="Player 1" size="large" color="success" value={player1} type="number" onChange={e => setPlayer1(+e.target.value)} />
+          <Button variant="contained" size="large" color="success" onClick={() => setPlayer1(current => current + 1)}>+1</Button>
+          <Button variant="contained" size="large" color="secondary" onClick={() => setPlayer1(current => current + 5)}>+5</Button>
+          <Button variant="contained" size="large" color="success" onClick={() => setPlayer1(current => current + 10)}>+10</Button>
+          <Button variant="contained" size="large" color="secondary" onClick={() => setPlayer1(current => current + 20)}>+20</Button>
+          <Button variant="contained" size="large" color="success" onClick={() => setPlayer1(current => current + 50)}>+50</Button>
         </Paper>
 
         <Paper>
-          <TextField label="Player 2" color="secondary" value={player2} type="number" onChange={e => setPlayer1(+e.target.value)} />
-          <Button variant="contained" color="success" onClick={() => setPlayer2(current => current + 1)}>+1</Button>
-          <Button variant="contained" color="success" onClick={() => setPlayer2(current => current + 5)}>+5</Button>
-          <Button variant="contained" color="success" onClick={() => setPlayer2(current => current + 10)}>+10</Button>
-          <Button variant="contained" color="success" onClick={() => setPlayer2(current => current + 20)}>+20</Button>
-          <Button variant="contained" color="success" onClick={() => setPlayer2(current => current + 50)}>+50</Button>
+          <TextField label="Player 2" color="secondary" value={player2} type="number" onChange={e => setPlayer2(+e.target.value)} />
+          <Button variant="contained" size="large" color="success" onClick={() => setPlayer2(current => current + 1)}>+1</Button>
+          <Button variant="contained" size="large" color="secondary" onClick={() => setPlayer2(current => current + 5)}>+5</Button>
+          <Button variant="contained" size="large" color="success" onClick={() => setPlayer2(current => current + 10)}>+10</Button>
+          <Button variant="contained" size="large" color="secondary" onClick={() => setPlayer2(current => current + 20)}>+20</Button>
+          <Button variant="contained" size="large" color="success" onClick={() => setPlayer2(current => current + 50)}>+50</Button>
         </Paper>
       </div>
-      <Button variant="contained" color="warning" onClick={() => { setPlayer1(0); setPlayer2(0) }}>Reset</Button>
+      <Button variant="contained" color="warning" size="large" onClick={() => { setPlayer1(0); setPlayer2(0) }}>Reset</Button>
 
     </div>
   );
